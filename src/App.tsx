479ca18fa5838f1d0ffb7c35e0d7bb2d60c7378c
Asad Dhorajiwala <asadrehandhorajiwala@gmail.com>
2022-05-18 16:17:53 +0530

    Working prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactMarkdown from 'react-markdown'
+import {Col, Container, FormControl, InputGroup, Row} from "react-bootstrap";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [input, setInput] = React.useState("")
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(event.target.value);
+    }
+
+    return (
+        <div className="App">
+            <Container fluid className="vh-100">
+                <Row className="h-100">
+                    <Col className="h-100 w-50 g-0">
+                        <InputGroup className="h-100">
+                            <FormControl className="rounded-0 shadow-none" style={{resize: "none"}} as="textarea"
+                                         value={input} onChange={handleChange}/>
+                        </InputGroup>
+                    </Col>
+                    <Col className="h-100 w-50">
+                        <ReactMarkdown children={input} remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}/>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
 }
 
 export default App;
